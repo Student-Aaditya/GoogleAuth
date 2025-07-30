cc9196584dba98bbaa411c0116a452dc7e2c9a1b
Aaditya <aadityakuamr518@gmail.com>
2025-07-30 23:54:25 +0530

    data entery

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback",
+    callbackURL: "https://googleauth-gyb6.onrender.com/auth/google/callback",
 }, async(accessToken, refreshToken, profile, done) => {
     try{
         let user = await User.findOne({ googleId: profile.id });
